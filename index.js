ae8243bc34213ca9a6a3d000af0326bd5021928a
Roman Karavia <3082173+rkaravia@users.noreply.github.com>
2020-11-29 10:48:42 +0100

    Skip upload if token not provided

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const config = {
   headfulness: process.env.HEADFULNESS,
 };
 
+const auth = process.env.GITHUB_TOKEN;
+
 (async () => {
   const matrix = `${config.os}_${config.browserType}_${config.headfulness}`;
   console.log(matrix);
 
   const screenshot = await capture(config, matrix);
-  await upload(screenshot, matrix);
+  
+  if (auth) {
+    await upload(screenshot, matrix, auth);
+  } else {
+    console.log("Skipping upload because GITHUB_TOKEN has not been provided.");
+  }
 })();
-
